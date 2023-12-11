36c0da2e1c53909a3ffefc1af0f7cc8b1007e23e
MrAbidRajpoot <abid.rajpo0t4741@gmail.com>
2023-12-11 23:28:40 +0500

    Create techareaSeeder.php

---
+++ database/seeders/techareaSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\techarea;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class techareaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run():void
+    {
+    
+    }
+}
