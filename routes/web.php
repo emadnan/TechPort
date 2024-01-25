509f8f359321cfed9cd666fb877796c5618471d0
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-25 12:50:03 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -280,6 +280,8 @@ Route::controller(TechReferredController::class)->group(function(){
     Route::get('/techPage' ,'techPage')->name('techPage');
     Route::post('/techreferredCreate' , 'create')->name('techreferredCreate');
     Route::get('/getTechArea' ,'getTechAreas')->name('getTechArea');
+    Route::get('/techReferredUpdatePage/{id}' , 'updatePage')->name('techReferredUpdatePage');
+    Route::post('/techReferredUpdate' , 'update')->name('techReferredUpdate');
 });
 
 Route::controller(ProjectTechnologyController::class)->group(function(){
