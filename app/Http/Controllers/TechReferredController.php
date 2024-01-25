c1408d14ae2228a42602cae1cf1380187bebd03b
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-25 12:49:04 +0500

    Update TechReferredController.php

--- app/Http/Controllers/TechReferredController.php
+++ app/Http/Controllers/TechReferredController.php
@@ -55,5 +55,53 @@ class TechReferredController extends Controller
         }
 
     }
+
+    public function updatePage(string $id)
+    {
+        $upArea = techreferred::where('id' , $id)->get();
+        return response()->json(['row'=>$upArea]);
+    }
+
+    public function update(Request $req)
+    {
+        $id = $req->id;
+        $validator = Validator::make($req->all(),[
+            'techarea'=> 'required',
+            'techsector'=> 'required',
+            'techniche'=> 'required',
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors(), 'oldInput' => $req->all()]);
+        }
+
+        $techreferred = techreferred::where('id' , $id);
+        $techreferred->id_techarea = $req->techarea;
+        $techreferred->id_techsector = $req->techsector;
+        $techreferred->id_techniche = $req->techniche;
+        $techreferred->save();
+
+        // $Update = DB::table('techsector')->where('id',$id)->update([
+        //     'techsector'=> $req->techsector,
+        //     'techsectordescription'=> $req->techsectordescription,
+        //     'id_dm'=> $req->id_dm,
+        //     'otme'=> $req->otme,
+        //     'note'=> $req->note,
+        // ]);
+
+        if($techreferred)
+        {
+            // $update = DB::table('techsector')->where('id', $id)->get();
+            // if($update)
+            // {
+              return response()->json(['message' => 'Technology Sector  Updated Successfully' , 'update'=>$techreferred]);
+            // }
+        }
+        else
+        {
+            return response()->json(['message' => 'Technology Sector Not Updated Successfully']);
+        }
+    }
+
  
 }
