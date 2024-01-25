2920c9f76cf06d5781c4b3e3168d92ff1cd8944e
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-25 14:49:43 +0500

    Update sidebar.blade.php

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -4,7 +4,7 @@
     }
 </style>
 
-<aside class="main-sidebar sidebar-dark-primary elevation-4" style="height:100%; overflow-y: scroll">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="height:100%; overflow-y: auto">
 
     <!-- Sidebar -->
     <div class="sidebar">
