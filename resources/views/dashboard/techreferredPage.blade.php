e5f95ae9072200722fa8e6548ddfead2c8399442
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-25 13:20:21 +0500

    Update techreferredPage.blade.php

--- resources/views/dashboard/techreferredPage.blade.php
+++ resources/views/dashboard/techreferredPage.blade.php
@@ -67,6 +67,26 @@
                 </div>
             </div>
         </div>
+<!-- Add techreferred Model -->
+
+<!--View Modal content-->
+
+<div class="modal fade " id="view-modal" role="dialog">
+    <div class="modal-dialog" style="left: -100px;">
+      <div class="modal-content" style="width: 160%;">
+        <div class="card-header">
+          <h4 class="modal-title">Technology Sector Details</h4>
+        </div>
+        <div class="modal-body" id="details">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+<!-- View Model End -->
 
 
 <div class="container" >
@@ -277,6 +297,63 @@
                 });
             }
         });
+
+
+        $('body').on('click' , '#view-btn' , function(){
+             $('#view-modal').modal('show');
+             var id = $(this).data('id');
+            var data = $('#details');
+            data.empty(); 
+            var url = 'techReferredRead/' + id;
+             $.ajax({
+                type: 'GET',
+                url: url,
+
+                success: function(response) {
+                   
+                    $.each(response.data , function(index , item){
+                        var descp ='<p>'+ 
+                '<span style="font-weight:700;">Technology Sector : </span>'
+                +item.id_techarea+ '</p>'+
+                '<p>'+ '<span  style="font-weight:700;">Description : </span>'
+                +item.id_techsector+'</p>'+
+                '<p>'+ '<span style="font-weight:700;"> ID_DM : </span>'+item.id_techniche+'</p>';
+                data.append(descp);
+                });
+
+                }
+             });
+        });
+
+
+        $('body').on('click' , '#delete-btn' , function(){
+    var id = $(this).data('id');
+    var url = 'techReferredDelete/' + id;
+    $.ajax({
+                type: 'GET',
+                url: url,
+
+                success: function(response) {
+                    $('#row_'+id).remove();
+                    
+                    var b_alert = 
+                        '<div id="alert" class="alert alert-danger alert-dismissible" >'+
+                            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">'+
+                                '<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>'+
+                            '</svg>'+
+                           '<span></span><span id="result"></span>'+
+                        '</div>';
+                        var column = $('#first_column');
+                        column.prepend(b_alert);
+                            $('#result').html(response.message);
+                            $('#alert').show();
+                            var alert = $('.alert')
+                        setTimeout(function() {
+                           alert.alert("close");
+                        }, 2000);
+            }
+    });
+});
 
 </script>
 @endsection
