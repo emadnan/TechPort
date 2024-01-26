35e2fa4a3c4cfa320e468bee9c5eafe18fdd0a91
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-26 10:29:31 +0500

    Update techSubSectorPage.blade.php

--- resources/views/dashboard/techSubSectorPage.blade.php
+++ resources/views/dashboard/techSubSectorPage.blade.php
@@ -207,18 +207,15 @@ $('#form').submit(function(){
 
                         if(response.errors)
                         {
-                            $("#technicheError").text(response.errors.techniche);
-                            $("#technichedescriptionError").text(response.errors.technichedescription);
-                            $("#id_dmError").text(response.errors.id_dm);
-                            $("#otmeError").text(response.errors.otme);
-                            $("#noteError").text(response.errors.note);
-
-                            $("#techniche").val(response.oldInput.techniche);
-                            $("#technichedescription").val(response.oldInput.technichedescription);
-                            $("#id_dm").val(response.oldInput.id_dm);
-                            $("#otme").val(response.oldInput.otme);
-                            $("#note").val(response.oldInput.note);
-
+                            var errorMessages = {
+                                  'otme': 'The Otme field is required.',
+                                  'techniche': 'The Technology Niche field is required.',
+                                  'technichedescription': 'The Description field is required.',
+                                };
+                                $.each(response.errors, function (key, value) {
+                                var customErrorMessage = errorMessages[key] || value;
+                                $('#' + key + 'Error').text(customErrorMessage);
+                                });
                         }
                         else
                         {
@@ -275,17 +272,15 @@ $('#form').submit(function(){
                     {
                         if(response.errors)
                         {
-                            $("#technicheError").text(response.errors.techniche);
-                            $("#technichedescriptionError").text(response.errors.technichedescription);
-                            $("#id_dmError").text(response.errors.id_dm);
-                            $("#otmeError").text(response.errors.otme);
-                            $("#noteError").text(response.errors.note);
-
-                            $("#techniche").val(response.oldInput.techniche);
-                            $("#technichedescription").val(response.oldInput.technichedescription);
-                            $("#id_dm").val(response.oldInput.id_dm);
-                            $("#otme").val(response.oldInput.otme);
-                            $("#note").val(response.oldInput.note);
+                            var errorMessages = {
+                                  'otme': 'The Otme field is required.',
+                                  'techniche': 'The Technology Niche field is required.',
+                                  'technichedescription': 'The Description field is required.',
+                                };
+                                $.each(response.errors, function (key, value) {
+                                var customErrorMessage = errorMessages[key] || value;
+                                $('#' + key + 'Error').text(customErrorMessage);
+                                });
                         }
                         else
                         {
