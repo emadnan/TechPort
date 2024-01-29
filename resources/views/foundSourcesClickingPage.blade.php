5f13527f9b80b57b386b140704e7e4e799fb9551
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 15:15:56 +0500

    Update foundSourcesClickingPage.blade.php

--- resources/views/foundSourcesClickingPage.blade.php
+++ resources/views/foundSourcesClickingPage.blade.php
@@ -350,10 +350,6 @@
         <ul class="breadCrumbs mt-2 mb-0">
             <li class="breadCrumbs-items"><a href="{{url('/')}}">Home</a></li>
             <li class="breadCrumbs-items">></li>
-            <li class="breadCrumbs-items"><a href="{{url('/search-results')}}">Search Results</a></li>
-            <li class="breadCrumbs-items">></li>
-            <li class="breadCrumbs-items"><a href="{{url('/low-evolution')}}">Project view</a></li>
-            <li class="breadCrumbs-items">></li>
             <li class="breadCrumbs-items"><a href="{{url('/found-sources')}}">Found Sources index</a></li>
             <li class="breadCrumbs-items">></li>
             <li class="breadCrumbs-items"><a href="{{url('/found-sources-clicking')}}">Found Sources view</a></li>
