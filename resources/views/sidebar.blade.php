b9bf703f063d82c87d26ab4c4256b74abc699a59
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 16:12:29 +0500

    Update sidebar.blade.php

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -50,7 +50,7 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item nav-dropdown">
+                <li class="nav-item">
                     <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     <p>
@@ -321,7 +321,3 @@
     </div>
     <!-- /.sidebar -->
 </aside>
-
-<script>
-    ()
-</script>
