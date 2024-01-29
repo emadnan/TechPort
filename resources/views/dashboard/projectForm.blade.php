4aed99343d0e9b044ffaeef144efa86203c5a6ef
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 17:18:59 +0500

    Update projectForm.blade.php

--- resources/views/dashboard/projectForm.blade.php
+++ resources/views/dashboard/projectForm.blade.php
@@ -285,7 +285,7 @@
 $(document).ready(function(){
     $(window).on('load', function() {
             $('#menu_projects').addClass('bg-primary');
-            $('#menu_projects').parent().parent().children().addClass('bg-primary');
+            $('#menu_projects').parent().parent().children('a:first-child').addClass('bg-primary');
     });
         $.ajaxSetup({
                 headers: { 
