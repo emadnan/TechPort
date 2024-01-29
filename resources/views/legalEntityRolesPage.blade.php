083851accc16baef44aad82f0aa6fed76a03b55e
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 15:12:49 +0500

    Update legalEntityRolesPage.blade.php

--- resources/views/legalEntityRolesPage.blade.php
+++ resources/views/legalEntityRolesPage.blade.php
@@ -305,10 +305,6 @@
         <ul class="breadCrumbs mt-2 mb-0">
             <li class="breadCrumbs-items"><a href="{{url('/')}}">Home</a></li>
             <li class="breadCrumbs-items">></li>
-            <li class="breadCrumbs-items"><a href="{{url('/search-results')}}">Search Results</a></li>
-            <li class="breadCrumbs-items">></li>
-            <li class="breadCrumbs-items"><a href="{{url('/low-evolution')}}">Project view</a></li>
-            <li class="breadCrumbs-items">></li>
             <li class="breadCrumbs-items"><a href="{{url('/legal-entity-roles')}}">Legal Entity Roles index</a></li>
         </ul>
         <!-- Add a new row for the image container -->
