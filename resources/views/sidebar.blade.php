bdc982a19a8ed3db38bcb0df4aeb51700e237904
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 16:14:31 +0500

    Update sidebar.blade.php

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -54,15 +54,15 @@
                     <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     <p>
-                        Dashboard
+                       Trajectory
                        <i class="right fas fa-angle-left"></i>
                     </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="./index.html" class="nav-link">
-                            <i class="far fa-circle nav-icon"></i>
-                            <p>Dashboard v1</p>
+                            <i class="fas fa-edit nav-icon"></i>
+                            <p>Projects</p>
                             </a>
                         </li>
                         <li class="nav-item">
