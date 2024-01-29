7b2c04be6a8c2632bd958764ae735fb5662881ca
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 14:47:30 +0500

    Update LocationController.php

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -24,6 +24,8 @@ class LocationController extends Controller
     ->where('id' , $id)
     ->first();
 
+    $location = location::find($id);
+
     $allTrls = trl::with('projects.trlactual')->get();
 
 
@@ -48,7 +50,7 @@ class LocationController extends Controller
 
 
 // return response()->json(compact('projOrg' , 'count' , 'active' , 'complete' , 'partnership') );
-        return view('locationClickingPage' , compact('projOrg' , 'count' , 'active' , 'complete' , 'partnership' , 'allTrls') );
+        return view('locationClickingPage' , compact('projOrg' , 'count' , 'active' , 'complete' , 'partnership' , 'allTrls' , 'location') );
     }
 
     public function getProjectsLengthByLocationID(string $locationID ,string $trlID)
