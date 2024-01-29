a20b80405b2cf36d3ae254be30e533545536616a
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 15:13:51 +0500

    Update projectTargetsPage.blade.php

--- resources/views/projectTargetsPage.blade.php
+++ resources/views/projectTargetsPage.blade.php
@@ -310,10 +310,6 @@
         <ul class="breadCrumbs mt-2 mb-0">
             <li class="breadCrumbs-items"><a href="{{url('/')}}">Home</a></li>
             <li class="breadCrumbs-items">></li>
-            <li class="breadCrumbs-items"><a href="{{url('/search-results')}}">Search Results</a></li>
-            <li class="breadCrumbs-items">></li>
-            <li class="breadCrumbs-items"><a href="{{url('/low-evolution')}}">Project view</a></li>
-            <li class="breadCrumbs-items">></li>
             <li class="breadCrumbs-items"><a href="{{url('/project-targets')}}">Project Target  index</a></li>
         </ul>
 
