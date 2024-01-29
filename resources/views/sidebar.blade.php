a9f070919d94ff94b6bbf7d3e3e290476855f863
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 16:28:55 +0500

    Update sidebar.blade.php

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -52,7 +52,7 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <i class="nav-icon fas fa-th"></i>
                     <p>
                        Trajectory
                        <i class="right fas fa-angle-left"></i>
@@ -109,7 +109,7 @@
 
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <i class="nav-icon fas fa-th"></i>
                     <p>
                        Entities
                        <i class="right fas fa-angle-left"></i>
