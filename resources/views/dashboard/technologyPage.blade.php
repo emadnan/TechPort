ec2bb01b170abd2841015bc5da98d09e42446d91
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 17:20:26 +0500

    Update technologyPage.blade.php

--- resources/views/dashboard/technologyPage.blade.php
+++ resources/views/dashboard/technologyPage.blade.php
@@ -118,6 +118,7 @@ $(document).ready(function(){
     $(window).on('load', function() {
         // Check if the element with ID "menu_techreferred" exists.
             $('#menu_technologies').addClass('bg-primary');
+            $('#menu_technologies').parent().parent().children('a:first-child').addClass('bg-primary');
     });
         $.ajaxSetup({
                 headers: { 
