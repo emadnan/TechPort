8e1172073b9a22d22a5c065848217cce506f9fb6
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-29 15:18:42 +0500

    Update foundSourcesClickingPage.blade.php

--- resources/views/foundSourcesClickingPage.blade.php
+++ resources/views/foundSourcesClickingPage.blade.php
@@ -352,7 +352,7 @@
             <li class="breadCrumbs-items">></li>
             <li class="breadCrumbs-items"><a href="{{url('/found-sources')}}">Found Sources index</a></li>
             <li class="breadCrumbs-items">></li>
-            <li class="breadCrumbs-items"><a href="#">{{$projOrg->name}}</a></li>
+            <li class="breadCrumbs-items"><b>{{$projOrg->name}}</b></li>
         </ul>
 
 @include('layouts.image')
