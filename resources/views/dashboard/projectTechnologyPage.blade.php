074299b972f2a1e618fd55444119ff7b6d442011
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-30 12:02:30 +0500

    Update projectTechnologyPage.blade.php

--- resources/views/dashboard/projectTechnologyPage.blade.php
+++ resources/views/dashboard/projectTechnologyPage.blade.php
@@ -92,6 +92,7 @@
                 </div>
 
                 <div class="card-body">
+                  <div class="table-responsive">
                     <table id="data-table" class="table table-bordered table-striped">
                         <thead>
                             <th class="py-1">Project</th>
@@ -115,6 +116,7 @@
                             @endforeach
                         </tbody>
                     </table>
+</div>
                     <div class="row justify-content-center py-2">
                         <div class="col-3" style="padding-left:40px;">
                         </div>
