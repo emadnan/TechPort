35afb37e30cf44d5fe0f88d4bcf959e057c01c66
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-30 11:49:11 +0500

    Update businessArea.blade.php

--- resources/views/dashboard/businessArea.blade.php
+++ resources/views/dashboard/businessArea.blade.php
@@ -220,7 +220,7 @@ $('#form').submit(function(){
                                 '<td class="py-1" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 200px; ">' + item.businessarea + '</td>' +
                                 '<td class="py-1" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 200px; ">' + item.description + '</td>'+ 
                                 '<td class="py-1" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 200px; ">'+item.note+'</td>'+
-                                '<td style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 90px; padding-top:15px;">'+
+                                '<td style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width: 80px; padding-top:15px;">'+
                                     '<i id="view-btn" class="fa-solid fa-eye fa-lg" style="color:#28A745; " data-id="'+item.id+'"> <span style="color:black; padding-right:4px;">|</span> </i>'+
                                     '<i id="update-btn" class="fa-solid fa-pen-to-square fa-lg" style="color:#E0A800;" data-id="'+item.id+'"> <span style="color:black; padding-right:4px;">|</span> </i>'+ 
                                     '<i id="delete-btn" class="fa-regular fa-trash-can fa-lg" style="color:#C82333;"  data-id="'+item.id+'"></i>'+
@@ -279,7 +279,7 @@ $('#form').submit(function(){
                                  '<td class="py-1" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 200px; ">' + item.businessarea + '</td>' +
                                  '<td class="py-1" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 200px; ">' + item.description + '</td>'+ 
                                  '<td class="py-1" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 200px; ">'+item.note+'</td>'+
-                                 '<td style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; max-width: 90px; padding-top:15px;">'+
+                                 '<td style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; width: 80px; padding-top:15px;">'+
                                     '<i id="view-btn" class="fa-solid fa-eye fa-lg" style="color:#28A745; " data-id="'+item.id+'"> <span style="color:black; padding-right:4px;">|</span> </i>'+
                                     '<i id="update-btn" class="fa-solid fa-pen-to-square fa-lg" style="color:#E0A800;" data-id="'+item.id+'"> <span style="color:black; padding-right:4px;">|</span> </i>'+ 
                                     '<i id="delete-btn" class="fa-regular fa-trash-can fa-lg" style="color:#C82333;"  data-id="'+item.id+'"></i>'+
