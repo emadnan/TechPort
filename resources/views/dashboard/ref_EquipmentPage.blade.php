4341edcd69997610fb1b38eb829e2287ce7e136b
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-30 12:02:46 +0500

    Update ref_EquipmentPage.blade.php

--- resources/views/dashboard/ref_EquipmentPage.blade.php
+++ resources/views/dashboard/ref_EquipmentPage.blade.php
@@ -87,6 +87,7 @@
                 </div>
 
                 <div class="card-body">
+                  <div class="table-responsive">
                     <table id="data-table" class="table table-bordered table-striped">
                         <thead>
                             <th class="py-1">Product</th>
@@ -107,6 +108,7 @@
                             @endforeach
                         </tbody>
                     </table>
+</div>
                     <div class="row justify-content-center py-2">
                         <div class="col-3" style="padding-left:40px;">
                         </div>
