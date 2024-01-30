916fb1a95df53c41f3290c89e4fbc0fa21027b89
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-30 12:03:30 +0500

    Update requirementPage.blade.php

--- resources/views/dashboard/requirementPage.blade.php
+++ resources/views/dashboard/requirementPage.blade.php
@@ -88,6 +88,7 @@
                 </div>
 
                 <div class="card-body">
+                  <div class="table-responsive">
                     <table id="data-table" class="table table-bordered table-striped">
                         <thead>
                             <th class="py-1">Reference</th>
@@ -110,6 +111,7 @@
                             @endforeach
                         </tbody>
                     </table>
+</div>
                     <div class="row justify-content-center py-2">
                         <div class="col-3" style="padding-left:40px;">
                         </div>
