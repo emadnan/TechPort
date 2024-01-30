253dbcd0f08bb689b1e0f1b4e2318f09dc8bbd4c
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-01-30 11:59:05 +0500

    Update humanEntityPage.blade.php

--- resources/views/dashboard/humanEntityPage.blade.php
+++ resources/views/dashboard/humanEntityPage.blade.php
@@ -107,6 +107,7 @@
                 </div>
 
                 <div class="card-body">
+                  <div class="table-responsive">
                     <table id="data-table" class="table table-bordered table-striped">
                         <thead>
                             <th class="py-1">Name</th>
@@ -135,6 +136,7 @@
                             @endforeach
                         </tbody>
                     </table>
+                   </div>
                     <div class="row justify-content-center py-2">
                         <div class="col-3" style="padding-left:40px;">
                         </div>
