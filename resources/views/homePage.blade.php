8fd1c6d973fc5c8806bdbd5d942f8eb01a765b71
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-01 17:30:23 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1721,7 +1721,7 @@ var techs = @json($techs);
 //   console.log('Draw Chart');
   var options = {
     chartArea: { width: '80%' , left:50 , right:20},
-    width: 150,
+    width: 250,
     height: 150,
     bar: {groupWidth: "60%"},
     legend: { position: "none" },
