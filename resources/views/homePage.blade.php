6a6415f2bfeba1814084b38d6d6806bc52e4a6ac
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-01 14:21:57 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1379,7 +1379,8 @@
     <div class="row border border-dark">
         <div style = 'width:30px'></div>
         <div class="border border-dark p-1">
-            <div> Technology Area <img src="{{ asset('images/icon-blue.png') }}" alt="" class="float-right graph-image"
+            <div> Technology Area</div> 
+            <div><img src="{{ asset('images/icon-blue.png') }}" alt="" class="float-right graph-image"
                             onclick="showimage(this)"></div>
             <div class="row" style = 'display: none;'>
                 <div class="col-md-1 border border-dark p-1"></div>
