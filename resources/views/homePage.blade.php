e62bea49861dea939b8df8cd14b748b9004c91b3
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-01 11:38:58 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1382,13 +1382,17 @@
         </div>
         <div class="col-md-10 border p-3">
         {{$tech-> techarea}}
+        <div class="row">
         <div class="col-md-1 border"></div>
         <div class="col-md-10 border">
+            <div class="row">
             <div class="col-md-1 border p-3"></div>
             <div class="col-md-10 border p-3"></div>
             <div class="col-md-1 border p-3"></div>
+            </div>
         </div>
         <div class="col-md-1 border"></div>
+        </div>
         </div>
         <div class="col-md-1 border p-3">
 
