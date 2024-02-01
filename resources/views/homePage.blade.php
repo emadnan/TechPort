cbe84fbe2295aaebb0574cf3882d19e1da6b8ddc
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-01 17:34:39 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1394,7 +1394,7 @@
                     <img src="{{ asset('images/icon-blue.png') }}" alt="" class="float-right graph-image" onclick="showimage(this)">
                     <div class=" graph-pop" style="display: none;  z-index:{{1000 - $loop->index}}; ">
                             <div class="row m-0" style=" width:100%; background: rgba(6, 83, 134, 1);">
-                                <div class="col-md-9 px-2" style="color: white;">
+                                <div class="col-md-9 px-2" style="color: white; font-size:12px;">
                                     <section>{{$tech-> techarea}}
                                     </section>
                                 </div>
