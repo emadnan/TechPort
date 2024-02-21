df3b82a037301dbd2b37e91c88bffa5ec1eae6de
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-21 09:57:30 +0500

    Update SearchReultsController.php

--- app/Http/Controllers/SearchReultsController.php
+++ app/Http/Controllers/SearchReultsController.php
@@ -29,7 +29,7 @@ class SearchReultsController extends Controller
         $complete = $projOrgs->where('status.status' , 'Completed')->count();
         $partnership = $projOrgs->where('status.status' , 'Partnership')->count();
         
-        // return response()->json(compact('projOrgs' , 'count' , 'active' , 'complete' , 'partnership'));
+        return response()->json(compact('projOrgs' , 'count' , 'active' , 'complete' , 'partnership'));
 
         return view('searchResultsPage' , compact('projOrgs' , 'count' , 'active' , 'complete' , 'partnership' , 'allTrls'));
     }
