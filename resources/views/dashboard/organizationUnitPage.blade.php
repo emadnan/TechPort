c1bc30a63aeb3341b0cc5afa004cc18dfa651ac2
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-28 16:10:41 +0500

    Update organizationUnitPage.blade.php

--- resources/views/dashboard/organizationUnitPage.blade.php
+++ resources/views/dashboard/organizationUnitPage.blade.php
@@ -140,6 +140,7 @@
         // Check if the element with ID "menu_techreferred" exists.
             $('#menu_organzationUnits').addClass('bg-primary');
             $('#menu_organzationUnits').parent().parent().children('a:first-child').addClass('bg-primary');
+            $('#menu_organzationUnits').parent().parent().addClass('menu-is-opening menu-open');
     });
         $.ajaxSetup({
                 headers: { 
