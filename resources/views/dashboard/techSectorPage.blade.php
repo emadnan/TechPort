20ed6b9ab5270037baf94e8dbe200962ba1651fb
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-28 11:59:08 +0500

    Update techSectorPage.blade.php

--- resources/views/dashboard/techSectorPage.blade.php
+++ resources/views/dashboard/techSectorPage.blade.php
@@ -50,9 +50,9 @@
                     </div>
                 </div>
 
-                <div id="techNicheDropDown" class="row mb-3">
+                <div  class="row mb-3">
                     <label for="id_techniche" class="col-md-4 col-form-label text-md-end">{{ __('Technolgy Niche') }}</label>
-                    <div class="col-md-6">
+                    <div id="techNicheDropDown" class="col-md-6">
                         <select id="id_techniche" type="text" class="form-control @error('id_techniche') is-invalid @enderror" name="id_techniche" value="{{ old('id_techniche') }}" autocomplete="id_techniche" autofocus required>
                             <option selected value="">SELECT</option>
                             @foreach ($niches as $niche )
@@ -164,20 +164,15 @@
                  }
             });
 
-    var newNiche = '<div class="col-md-6">'+
-                        '<select id="id_techniche" type="text" class="form-control @error('id_techniche') is-invalid @enderror" name="id_techniche" value="{{ old('id_techniche') }}" autocomplete="id_techniche" autofocus required>'+
+    var newNiche ='<select id="id_techniche" type="text" class="form-control @error('id_techniche') is-invalid @enderror" name="id_techniche" value="{{ old('id_techniche') }}" autocomplete="id_techniche" autofocus required>'+
                             '<option selected value="">SELECT</option>'+
                            ' @foreach ($niches as $niche )'+
                             '<option value="{{$niche-> id}}">{{$niche->techniche}}</option>'+
                             '@endforeach'+
-                        '</select>'+
-                    '</div>'+
-                    '<div class="col-md-2 p-0">'+
-                     '  <button class="btn btn-primary btn-sm addMoreNiche">+</button>'+
-                   ' </div>';
+                        '</select>';
     
     $('.addMoreNiche').click(function(){
-        $(this).hide();
+        // $(this).hide();
         $('#techNicheDropDown').append(newNiche);
     });
 
