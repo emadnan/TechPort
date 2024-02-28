8fab3ddd7483dc4a1df14b47491449391e565dbf
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-28 15:06:33 +0500

    Update TechSectorController.php

--- app/Http/Controllers/TechSectorController.php
+++ app/Http/Controllers/TechSectorController.php
@@ -99,6 +99,7 @@ class TechSectorController extends Controller
     public function updatePage(string $id)
     {
         $upArea = DB::table('techsector')->where('id', $id)->get();
+
         return response()->json(['row'=>$upArea]);
     }
 
@@ -108,6 +109,7 @@ class TechSectorController extends Controller
         $validator = Validator::make($req->all(),[
             'techsector'=> 'required',
             'techsectordescription'=> 'required',
+            'id_techniche'=> 'required',
             'id_dm'=> 'nullable',
             'otme'=> 'required',
             'note'=> 'nullable',
@@ -117,26 +119,52 @@ class TechSectorController extends Controller
             return response()->json(['errors' => $validator->errors(), 'oldInput' => $req->all()]);
         }
 
-        $Update = DB::table('techsector')->where('id',$id)->update([
-            'techsector'=> $req->techsector,
-            'techsectordescription'=> $req->techsectordescription,
-            'id_dm'=> $req->id_dm,
-            'otme'=> $req->otme,
-            'note'=> $req->note,
-        ]);
+        // $Update = DB::table('techsector')->where('id',$id)->update([
+        //     'techsector'=> $req->techsector,
+        //     'techsectordescription'=> $req->techsectordescription,
+        //     'id_dm'=> $req->id_dm,
+        //     'otme'=> $req->otme,
+        //     'note'=> $req->note,
+        // ]);
 
-        if($Update)
-        {
-            $update = DB::table('techsector')->where('id', $id)->get();
-            if($update)
-            {
-              return response()->json(['message' => 'Technology Sector  Updated Successfully' , 'update'=>$update]);
+        $selectedTechNiches = $req->selected_techniche;
+        $techreferreds = techreferred::where('id_techsector' , $id)->get();
+        if(is_array($selectedTechNiches)) {
+            if(count($selectedTechNiches) < count($techreferreds)) {
+                $count = count($techreferreds) - count($selectedTechNiches);
+                for ($i=0 ; $i<=$count ; $i++) {
+                    $techreferreds[$i]->delete();
+                }                
+            }
+            // else if(count($selectedTechNiches) > count($techreferreds))
+            // for($i=0 ; $i<=count($techreferreds) ; $i++) {
+            //     $techreferred[$i]->id_techniche = $selectedTechNiches[$i];
+            // }
+        }
+        else {
+            $selectedTechnicheArray = explode(',', $selectedTechNiches);
+            if(count($selectedTechnicheArray) < count($techreferreds)) {
+                $count = count($techreferreds) - count($selectedTechnicheArray);
+                for ($i=0 ; $i<=$count ; $i++) {
+                    $techreferreds[$i]->delete();
+                }                
             }
         }
-        else
-        {
+
             return response()->json(['message' => 'Technology Sector Not Updated Successfully']);
-        }
+
+        // if($Update)
+        // {
+        //     $update = DB::table('techsector')->where('id', $id)->get();
+        //     if($update)
+        //     {
+        //       return response()->json(['message' => 'Technology Sector  Updated Successfully' , 'update'=>$update]);
+        //     }
+        // }
+        // else
+        // {
+        //     return response()->json(['message' => 'Technology Sector Not Updated Successfully']);
+        // }
     }
 
     public function delete(string $id)
