6b188f5299ac804bd2b4418afae97fd98e5e8644
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-02-28 16:11:12 +0500

    Update orgPerformingWorkPage.blade.php

--- resources/views/dashboard/orgPerformingWorkPage.blade.php
+++ resources/views/dashboard/orgPerformingWorkPage.blade.php
@@ -188,6 +188,7 @@
         // Check if the element with ID "menu_techreferred" exists.
             $('#menu_workingOrganizations').addClass('bg-primary');
             $('#menu_workingOrganizations').parent().parent().children('a:first-child').addClass('bg-primary');
+            $('#menu_workingOrganizations').parent().parent().addClass('menu-is-opening menu-open');
     });
         $.ajaxSetup({
                 headers: { 
