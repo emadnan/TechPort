97a27490da7346d3fcb339616e1b6131e22dfaa8
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-03-04 17:09:05 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1655,15 +1655,15 @@
             sector_text.html(highlightedText);
         });
 
-        // $('.tech_niche').each(function(){
-        //     var area_text = $(this).find('.area_text');
-        //     var original_text = area_text.text();
-        //     var text = area_text.text().toLowerCase();
-        //     var highlightedText = original_text.replace(new RegExp(search, 'gi'), function(match) {
-        //         return '<span class="highlight">' + match + '</span>';
-        //     });
-        //     area_text.html(highlightedText);
-        // });
+        $('.tech_niche').each(function(){
+            var niche_text = $(this).find('.niche_text');
+            var original_niche_text = area_text.text();
+            var nicheText = niche_text.text().toLowerCase();
+            var highlightedText = original_niche_text.replace(new RegExp(search, 'gi'), function(match) {
+                return '<span class="highlight">' + match + '</span>';
+            });
+            niche_text.html(highlightedText);
+        });
 
         // $('.tech_area').each(function(){
         //     var area_text = $(this).find('.area_text');
