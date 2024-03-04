a4bd4d3b89dbf9c2ade2db038ec36f790a1a35f9
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-03-04 16:54:28 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1636,6 +1636,11 @@
           var search = $(this).val().toLowerCase();
           $('.tech_area').each(function(){
             var area_text = $(this).find('.area_text');
+            var sector_text = $(this).find('.sector_text');
+            var niche_text = $(this).find('.niche_text');
+            console.log(area_text);
+            console.log(sector_text);
+            console.log(niche_text);
             var original_text = area_text.text();
             var text = area_text.text().toLowerCase();
             var highlightedText = original_text.replace(new RegExp(search, 'gi'), function(match) {
@@ -1644,6 +1649,31 @@
             area_text.html(highlightedText);
             $(this).toggle(text.indexOf(search) > -1);
         });
+
+        // $('.tech_area').each(function(){
+        //     var area_text = $(this).find('.area_text');
+        //     var sector_text = $(this).find('.sector_text');
+        //     var niche_text = $(this).find('.niche_text');
+        //     var original_area_text = area_text.text();
+        //     var original_sector_text = sector_text.text();
+        //     var original_niche_text = niche_text.text();
+        //     var areaText = area_text.text().toLowerCase();
+        //     var sectorText = sector_text.text().toLowerCase();
+        //     var nicheText = niche_text.text().toLowerCase();
+        //     var highlightedAreaText = original_area_text.replace(new RegExp(search, 'gi'), function(match) {
+        //         return '<span class="highlight">' + match + '</span>';
+        //     });
+        //     var highlightedSectorText = original_sector_text.replace(new RegExp(search, 'gi'), function(match) {
+        //         return '<span class="highlight">' + match + '</span>';
+        //     });
+        //     var highlightedNicheText = original_niche_text.replace(new RegExp(search, 'gi'), function(match) {
+        //         return '<span class="highlight">' + match + '</span>';
+        //     });
+        //     area_text.html(highlightedAreaText);
+        //     sector_text.html(highlightedSectorText);
+        //     sector_text.html(highlightedNicheText);
+        //     $(this).toggle(areaText.indexOf(search) > -1);
+        // });
         
   
   if ($('.tech_area:visible').length > 1) {
