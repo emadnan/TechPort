21fb87ca00ffa6fb7a7be968fbd9869718d83bb4
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-03-05 10:54:07 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1638,11 +1638,15 @@
             var area_text = $(this).find('.area_text');
             var original_text = area_text.text();
             var text = area_text.text().toLowerCase();
+            var sector_text = $(this).find('.sector_text');
+            var original_sector_text = sector_text.text();
+            var sectorText = sector_text.text().toLowerCase();
             var highlightedText = original_text.replace(new RegExp(search, 'gi'), function(match) {
                 return '<span class="highlight">' + match + '</span>';
             });
             area_text.html(highlightedText);
             $(this).toggle(text.indexOf(search) > -1);
+            $(this).toggle(sectorText.indexOf(search) > -1);
         });
 
         $('.tech_sector').each(function(){
@@ -1653,7 +1657,7 @@
                 return '<span class="highlight">' + match + '</span>';
             });
             sector_text.html(highlightedText);
-            $('.tech_area').toggle(sectorText.indexOf(search) > -1);
+            // $('.tech_area').toggle(sectorText.indexOf(search) > -1);
         });
 
         $('.tech_niche').each(function(){
