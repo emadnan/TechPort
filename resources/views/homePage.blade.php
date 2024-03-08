d8bab60c3d6de2cbc845ee9276a1a4b04d090391
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-03-08 15:14:22 +0500

    Update homePage.blade.php

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1499,7 +1499,7 @@
                        </div>
                         <div class="col p-0">
                             <div style="display:flex;  justify-content: space-between;  align-items: center;">
-                                <div class="pl-2 niche_text">TX0{{$sectorLoop}}.{{$loop->index+1}}{{$techniche-> techniche}}</div> 
+                                <div class="pl-2 niche_text">TX0{{$areaLoop}}.{{$sectorLoop}}.{{$loop->index+1}}{{$techniche-> techniche}}</div> 
                                 <div>
                                     <img src="{{ asset('images/icon-black.png') }}" alt="" class="float-right graph-image" onclick="showimage(this)">
                                     <div class="graph-pop" style="display: none;  z-index:{{998 - $loop->index}};">
