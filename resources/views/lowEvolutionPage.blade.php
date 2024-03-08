ecaf9dd688cfe84e241266ecc036ce17b1279fc2
Abid Rajpoot <147133204+MrAbidRajpoot@users.noreply.github.com>
2024-03-08 12:50:45 +0500

    Update lowEvolutionPage.blade.php

--- resources/views/lowEvolutionPage.blade.php
+++ resources/views/lowEvolutionPage.blade.php
@@ -305,16 +305,20 @@
 
                                 <table class="mb-5">
                                     <tr>
-                                        <th><a href="{{ url('/organizations') }}" style="color:#fff"> Organisation
+                                        <th><a href="{{ url('/organizations') }}" style="color:#fff"> Organization
                                                 Performing Work</a></th>
-                                        <th>Code</th>
+                                        <th>Type of Company</th>
                                         <th><a href="{{ url('/locationsPage') }}" style="color:#fff;">Location</a></th>
                                         <th>Description</th>
                                     </tr>
                            @foreach ( $projOrg->orgperformingworks as $org)
                                     <tr>
                                         <td><a href="{{ route('organizationClickingPage', ['id' => $org->id]) }}" style="color:black;">{{$org->name}}</a></td>
-                                        <td>{{$org->code}}</td>
+                            @endforeach
+                            @foreach ( $projOrg->legalentityroles as $role)
+                                        <td>{{$role->code}}</td>
+                            @endforeach
+                            @foreach ( $projOrg->orgperformingworks as $org)
                                         <td><a href="{{ route('locationsClickingPage', ['id' => $org->location->id]) }}" style="color:black;">{{$org->location->state}},{{$org->location->city}}</a></td>
                                         <td>{{$org->description}}</td>
                                     </tr>
